d02d689cd72392ca7d780c02b14c1e7f209eb8c6
JonTron23 <82520237+JonTron23@users.noreply.github.com>
2022-05-25 11:36:37 +0200

    login (not finished)

--- login.php
+++ login.php
@@ -13,8 +13,24 @@ include('db-connector.inc.php');
     <?php
     session_start();
 
+    $error = '';
     $email = $_POST['email'];
     $password = $_POST['password'];
+    $query = 'select email, password from users where email = ?';
+    $stmt = $mysqli->prepare($query);
+
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+        $email=trim($_POST["email"]);
+        $password=trim($_POST["password"]);
+
+        if(isset($email, $password)){
+            $stmt->bind_param('ss', $email, $password);
+            $stmt->execute();
+
+            
+        }
+
+    }
 
     if($user !== false && password_verify($password, $user['password'])) {
         $_SESSION['userid'] = $user['id'];
